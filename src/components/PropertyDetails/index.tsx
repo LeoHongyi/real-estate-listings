dc6d7cd93799cc597a1fb6750a02784ebccfaebe
Clifford Wolf <wenhualiu.liu@gmail.com>
2024-08-05 13:36:25 +0800

    remove useless

--- src/components/PropertyDetails/index.tsx
+++ src/components/PropertyDetails/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { usePropertyContext } from '../../context/PropertyContext';
 import { Link, useParams } from 'react-router-dom';
 import { PropertyDetailsSkeleton } from '../PropertyDetailsSkeleton';
